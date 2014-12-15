5a8e2d854738be62899cea4cd0e9a12d9ac0fd09
bblinder <brandon.blinderman@gmail.com>
2014-12-15 11:13:30 -0500

    Some syntax

--- bash_command.sh
+++ bash_command.sh
@@ -2,4 +2,4 @@
 egrep -i '(error)|(fail)|(rtmp://)|(port)|(\!)\|(drop)|(stamp)|(lost)|(connect)|(connection)|(auth)|(slow)|(check)|(quality)|(suggestion)'
 
 # Spits out the filtered search terms onto a doc, opens it
-cat > /Users/brandon/Documents/Logs.txt && open /Users/brandon/Documents/Logs.txt && echo "All Done!\nRemember, this is to help narrow down possible causes.\nIt is highly recommended that you view the entire log file - you may otherwise miss crucial context."
+cat > /Users/brandon/Documents/Logs.txt && open /Users/brandon/Documents/Logs.txt && echo "All Done!\nRemember, this is to help narrow down possible causes.\nIt is highly recommended that you view the entire log file - you might otherwise miss crucial context."
