2c0b6994ca03bb2eb493b5c229784749b780e116
bblinder <brandon.blinderman@gmail.com>
2014-12-15 15:08:35 -0500

    Added more commentary

--- bash_command.sh
+++ bash_command.sh
@@ -1,5 +1,7 @@
 # searches for the selected terms
 egrep -i '(error)|(fail)|(rtmp://)|(port)|(\!)\|(drop)|(stamp)|(lost)|(connect)|(connection)|(auth)|(slow)|(check)|(quality)|(suggestion)'
 
-# Spits out the filtered search terms onto a doc, opens it
+# Spits out the filtered search terms onto a doc, opens it.
+# Simply replace the /Path/To/File.txt with one relevant to your own system.
+
 cat > /Users/brandon/Documents/Logs.txt && open /Users/brandon/Documents/Logs.txt && echo "All Done!\nRemember, this is to help narrow down possible causes.\nIt is highly recommended that you view the entire log file - you might otherwise miss crucial context."
