cb2c9ba7e260dfae81a7cd29572ea3d60050ff1c
bblinder <brandon.blinderman@gmail.com>
2014-12-16 14:51:10 -0500

    Changed directory to /Tmp folder

--- bash_command.sh
+++ bash_command.sh
@@ -4,4 +4,5 @@ egrep -i '(error)|(fail)|(rtmp://)|(port)|(\!)\|(drop)|(stamp)|(lost)|(connect)|
 # Spits out the filtered search terms onto a doc, opens it.
 # Simply replace the /Path/To/File.txt with one relevant to your own system.
 
-cat > /Users/brandon/Documents/Logs.txt && open /Users/brandon/Documents/Logs.txt && echo "All Done!\nRemember, this is to help narrow down possible causes.\nIt is highly recommended that you view the entire log file - you might otherwise miss crucial context."
+cat > /tmp/Logs.txt && open /tmp/Logs.txt && echo "All done.\nRemember, this is to help narrow down possible causes.\nIt is highly recommended that you view the entire log file - you might otherwise miss crucial context."
+
